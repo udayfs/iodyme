3f397e692289100104c8357ca45835bfae837050
Uday Tiwari <udaytiwari360@outlook.com>
2025-07-19 12:45:53 +0530

    Add: next-theme support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { INTER_FONT } from "@/app/fonts";
+import ThemeProvider from "@/components/theme-provider";
 
 function RootLayout({
   children,
@@ -7,8 +8,17 @@ function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${INTER_FONT.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${INTER_FONT.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,10 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+
 import { Search, Menu, Briefcase, Building2, User, Bell } from "lucide-react";
+import ThemeToggle from "@/components/theme-toggle";
 
 import {
   DropdownMenu,
@@ -30,20 +33,25 @@ const navigations = [
   { name: "Guide", href: "/guide" },
 ];
 
-export function Navbar() {
+function Navbar({ className }: { className?: string }) {
   const [isOpen, setIsOpen] = useState(false);
 
   // TODO: Auth state (will be using clerk).
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 dark:backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <nav
+      className={cn(
+        "sticky top-0 z-50 w-full border-b bg-background/95 dark:backdrop-blur supports-[backdrop-filter]:bg-background/60",
+        className
+      )}
+    >
       <div className="container mx-auto px-4">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <div className="flex items-center mr-8 pb-1">
             <Link href="/" className="flex items-center space-x-2">
-              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary text-primary-foreground">
+              <div className="flex h-8 w-8 items-center justify-center rounded-xl bg-primary text-primary-foreground">
                 <Briefcase className="h-4 w-4" />
               </div>
               <span className="text-xl font-bold text-primary">iodyme</span>
@@ -70,7 +78,7 @@ export function Navbar() {
               <Input
                 type="search"
                 placeholder="Search jobs, companies..."
-                className="pl-10 pr-4 rounded-full"
+                className="pl-10 pr-4 rounded-xl"
               />
             </div>
           </div>
@@ -79,7 +87,7 @@ export function Navbar() {
             {/* Post Job Button */}
             <Button
               variant="outline"
-              className="inline-flex bg-transparent hover:cursor-pointer mr-4 rounded-full"
+              className="inline-flex bg-transparent hover:cursor-pointer mr-4 rounded-xl"
             >
               <Building2 className="h-4 w-4" />
               Post Job
@@ -146,83 +154,97 @@ export function Navbar() {
               <div className="hidden lg:flex items-center ml-4 space-x-2">
                 <Button
                   variant="outline"
-                  className="rounded-full hover:cursor-pointer"
+                  className="rounded-xl hover:cursor-pointer"
                   onClick={() => setIsLoggedIn(true) /* TODO: remove this. */}
                 >
                   Sign In
                 </Button>
                 <Button
-                  className="rounded-full hover:cursor-pointer"
+                  className="rounded-xl hover:cursor-pointer"
                   onClick={() => setIsLoggedIn(true) /* TODO: remove this. */}
                 >
                   Sign Up
                 </Button>
               </div>
             )}
+
+            {/* Desktop theme toggle */}
+            <ThemeToggle />
           </div>
 
-          {/* Mobile Menu */}
-          <Sheet modal open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger asChild>
-              <Button variant="ghost" className="lg:hidden" size="icon">
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle menu</span>
-              </Button>
-            </SheetTrigger>
+          <div className="flex lg:hidden items-center space-x-2">
+            {/* Mobile theme toggle */}
+            <ThemeToggle />
 
-            <SheetContent side="right" className="w-[300px] sm:w-[400px]">
-              <SheetHeader className="mb-[-10]">
-                <SheetTitle asChild>
-                  {/* Mobile Search */}
-                  <div className="relative top-[-6] mr-6">
-                    <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-                    <Input
-                      type="search"
-                      placeholder="Search jobs, companies..."
-                      className="pl-10 font-light"
-                    />
-                  </div>
-                </SheetTitle>
-              </SheetHeader>
+            {/* Mobile Menu */}
+            <Sheet modal open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="lg:hidden rounded-xl"
+                  size="icon"
+                >
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle menu</span>
+                </Button>
+              </SheetTrigger>
 
-              {/* Mobile Navigation */}
-              <div className="grid flex-1 auto-rows-min px-2">
-                <div className="grid gap-3">
-                  {navigations.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 text-base font-medium text-muted-foreground hover:text-primary"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                  <Button
-                    variant="outline"
-                    className="mt-3 ml-2 rounded-lg hover:cursor-pointer"
-                  >
-                    <Building2 className="mr-2 h-4 w-4" />
-                    Post Job
-                  </Button>
-
-                  {/* Auth Buttons */}
-                  {!isLoggedIn && (
-                    <div className="grid gap-2">
-                      <Button
-                        className="mt-1 ml-2 rounded-lg hover:cursor-pointer"
-                        onClick={() => setIsLoggedIn(true)}
-                      >
-                        Sign In
-                      </Button>
+              <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+                <SheetHeader className="mb-[-10]">
+                  <SheetTitle asChild>
+                    {/* Mobile Search */}
+                    <div className="relative top-[-6] mr-6">
+                      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+                      <Input
+                        type="search"
+                        placeholder="Search jobs, companies..."
+                        className="pl-10 font-light rounded-xl"
+                      />
                     </div>
-                  )}
+                  </SheetTitle>
+                </SheetHeader>
+
+                {/* Mobile Navigation */}
+                <div className="grid flex-1 auto-rows-min px-2">
+                  <div className="grid gap-3">
+                    {navigations.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 text-base font-medium text-muted-foreground hover:text-primary"
+                        onClick={() => setIsOpen(false)}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                    <Button
+                      variant="outline"
+                      className="mt-3 ml-2 rounded-xl hover:cursor-pointer"
+                    >
+                      <Building2 className="mr-2 h-4 w-4" />
+                      Post Job
+                    </Button>
+
+                    {/* Auth Button */}
+                    {!isLoggedIn && (
+                      <div className="grid gap-2">
+                        <Button
+                          className="mt-1 ml-2 rounded-xl hover:cursor-pointer"
+                          onClick={() => setIsLoggedIn(true)}
+                        >
+                          Sign In
+                        </Button>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </SheetContent>
-          </Sheet>
+              </SheetContent>
+            </Sheet>
+          </div>
         </div>
       </div>
     </nav>
   );
 }
+
+export default Navbar;
